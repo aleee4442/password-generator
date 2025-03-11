905f1c18fb7342e97c977a2467e1d1a5ebaa23be
aleee4442 <alejandrogonmillon@gmail.com>
2025-03-11 17:28:33 +0100

    User can now choose the password lenght

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,41 @@
-// Librerias Utilizadas
+// libraries used
 use rand::Rng; 
 use std::io;
 
-fn generar_contra(){
-    let mut rng = rand::rng();
+fn generate_password(lenght: u32){
+    let mut rng = rand::rng(); // for the random number generator
 
-    // vector con todos los
-    let digitos: Vec<char> = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+-=[]{}|;:,.<>?".chars().collect();
+    // all the possible characters in the password
+    let digits: Vec<char> = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+-=[]{}|;:,.<>?".chars().collect(); 
 
-    // Generar la contraseña
-    let mut contra = String::new();
-    for _ in 0..16 {
-        let indice = rng.random_range(0..digitos.len());
-        contra.push(digitos[indice]);
+    // generating the password
+    let mut password = String::new();
+    for _ in 0..lenght{
+        let indice = rng.random_range(0..digits.len()); // using digits len you can add more digits with no more changes
+        password.push(digits[indice]);
     }
-    println!("{}",contra);
+    println!("{}",password); // shows the generated password
 }
 
 
 fn main() {
-    let mut opcion = String::new();
-    println!("Bienvenido al generado de contraseñas seguras");
-    println!("Introduce el numero de contrasenias que quieres generar");
-    io::stdin().read_line(&mut opcion).expect("Error reading the line");
-    let opcion_u32 = opcion.trim().parse::<u32>().expect("Error, numero no introducido");
-    for _ in 0..opcion_u32{
-        generar_contra();
+    // variable for selecting the number of passwords to generate
+    let mut password_number = String::new(); 
+    
+    println!("Welcome to the safe password generator");
+    
+    println!("Input the number of passwords to generate");
+    io::stdin().read_line(&mut password_number).expect("Error reading the line");
+    let opcion_u32 = password_number.trim().parse::<u32>().expect("Error, number not introduced");
+    
+    for i in 0..opcion_u32{
+        let mut lenght_inserted = String::new();
+        
+        println!("GGenerating password {}", i+1);
+        println!("Choose the lenght of the password");
+        io::stdin().read_line(&mut lenght_inserted).expect("Error reading the line");
+        let input_lenght = lenght_inserted.trim().parse::<u32>().expect("Error, number not introduced");
+        generate_password(input_lenght);
     }
 }
-
-
-// prueba
-
 
